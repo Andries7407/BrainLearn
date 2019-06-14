632555e39641d0fa0b5f9c2d8ce30f37855e3645
amchess <amchess@libero.it>
2019-06-15 00:45:40 +0200

    1.0

--- src/makeAll.sh
+++ src/makeAll.sh
@@ -5,40 +5,40 @@ make clean
 
 make build ARCH=x86-64-modern COMPCC=gcc-7.3.0
 strip stockfish
-mv 'stockfish' 'BrainLearn250519-x86-64-modern'
+mv 'stockfish' 'BrainLearn1.0-x86-64-modern'
 make clean
 
 make build ARCH=x86-64-bmi2 COMPCC=gcc-7.3.0
 strip stockfish
-mv 'stockfish' 'BrainLearn250519-x86-64-bmi2'
+mv 'stockfish' 'BrainLearn1.0-x86-64-bmi2'
 make clean
 
 make build ARCH=x86-32 COMPCC=gcc-7.3.0
 strip stockfish
-mv 'stockfish' 'BrainLearn250519-x86-32'
+mv 'stockfish' 'BrainLearn1.0-x86-32'
 make clean
 
 make build ARCH=x86-32-old COMPCC=gcc-7.3.0
 strip stockfish
-mv 'stockfish' 'BrainLearn250519-x86-32-old'
+mv 'stockfish' 'BrainLearn1.0-x86-32-old'
 make clean
 
 make build ARCH=ppc-64 COMPCC=gcc-7.3.0
 strip stockfish
-mv 'stockfish' 'BrainLearn250519-ppc-64'
+mv 'stockfish' 'BrainLearn1.0-ppc-64'
 make clean
 
 make build ARCH=ppc-32 COMPCC=gcc-7.3.0
 strip stockfish
-mv 'stockfish' 'BrainLearn250519-ppc-32'
+mv 'stockfish' 'BrainLearn1.0-ppc-32'
 make clean
 
 make build ARCH=general-64 COMPCC=gcc-7.3.0
 strip stockfish
-mv 'stockfish' 'BrainLearn250519-general-64'
+mv 'stockfish' 'BrainLearn1.0-general-64'
 make clean
 
 make build ARCH=general-32 COMPCC=gcc-7.3.0
 strip stockfish
-mv 'stockfish' 'BrainLearn250519-general-32'
+mv 'stockfish' 'BrainLearn1.0-general-32'
 make clean
--- src/misc.cpp
+++ src/misc.cpp
@@ -56,7 +56,7 @@ namespace {
 
 /// Version number. If Version is left empty, then compile date in the format
 /// DD-MM-YY and show in engine_info.
-const string Version = "";
+const string Version = "1.0";
 
 /// Our fancy logging facility. The trick here is to replace cin.rdbuf() and
 /// cout.rdbuf() with two Tie objects that tie cin and cout to a file stream. We
@@ -138,7 +138,7 @@ const string engine_info(bool to_uci) {
   ss << (Is64Bit ? " 64" : "")
      << (HasPext ? " BMI2" : (HasPopCnt ? " POPCNT" : ""))
      << (to_uci  ? "\nid author ": " by ")
-     << "T. Romstad, M. Costalba, J. Kiiski, G. Linscott";
+     << "K. Kinyama, A. Manzo";
 
   return ss.str();
 }
